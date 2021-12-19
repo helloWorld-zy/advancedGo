6fe9b08b7e81a8e9e6cffdd41f2a04a85006a5e3
helloWorld-zy <71317215+helloWorld-zy@users.noreply.github.com>
2021-12-19 22:57:05 +0800

    Add files via upload

---
+++ httpServer.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+func main() {
+	g, ctx := errgroup.WithContext(context.Background())
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ping"))
+	})
+
+	serverOut := make(chan struct{})
+	mux.HandleFunc("/shutdown", func(w http.ResponseWriter, r *http.Request) {
+		serverOut <- struct{}{}
+	})
+
+	server := http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+
+	g.Go(func() error {
+		return server.ListenAndServe()
+	})
+
+	g.Go(func() error {
+		select {
+		case <-ctx.Done():
+			log.Println("Exit")
+		case <-serverOut:
+			log.Println("Server Out")
+		}
+
+		timeoutCtx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+
+		log.Println("Shutting Down Server")
+		return server.Shutdown(timeoutCtx)
+	})
+
+	g.Go(func() error {
+		quit := make(chan os.Signal, 0)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case sig := <-quit:
+			return fmt.Errorf("OS Signal: %v", sig)
+		}
+	})
+
+	fmt.Printf("Exiting: %+v\n", g.Wait())
+}
